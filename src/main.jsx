4f9dcb588db19b21e92603776b096f56604a2c78
Ronald De Mesa <ronaldreyjr061201@gmail.com>
2025-08-28 22:55:25 +0800

    add routing for pages

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,26 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./App.jsx";
+import { createBrowserRouter, RouterProvider } from "react-router";
+import LandingPage from "./components/LandingPage.jsx";
+import TestPage from "./components/TestPage.jsx";
 
-createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <LandingPage />,
+  },
+  {
+    path: "/words",
+    element: <TestPage />,
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router}>
+      <App />
+    </RouterProvider>
+  </StrictMode>
+);
