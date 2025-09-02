57ab0244ea559cf8e9c05dee4b79fa2f4451ba3f
Enriko Espinosa <2200427@slu.edu.ph>
2025-09-02 15:11:12 +0800

    add className classes based on bootstrap library

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,5 +1,9 @@
 import React from "react";
+import { NavLink } from "react-router";
 
 export default function Footer() {
-    return <footer>Footer</footer>;
+    return <footer className="w-full bg-secondary">
+        <NavLink to={'/words'} className="px-2 text-white rounded">Words</NavLink>
+        Footer
+        </footer>;
 }
