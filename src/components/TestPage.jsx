9d034ac89f2c116157bc6283e7417c4d1d373d58
Enriko Espinosa <2200427@slu.edu.ph>
2025-09-02 14:06:46 +0800

    Initial footer jsx

--- src/components/TestPage.jsx
+++ src/components/TestPage.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import About from "./About";
 import Table from "./Table";
+import Footer from "./Footer";
 
 function TestPage() {
   return (
     <div>
       <About />
       <Table />
+      <Footer />
     </div>
   );
 }
