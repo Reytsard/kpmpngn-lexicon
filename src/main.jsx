40f2be39c2e466b00733f76f1d8c24f438ddcf5c
Ronald De Mesa <ronaldreyjr00601201@gmail.com>
2025-09-05 13:45:38 +0800

    add default layout for the lexicon

--- src/main.jsx
+++ src/main.jsx
@@ -3,25 +3,25 @@ import { createRoot } from "react-dom/client";
 import "./index.css";
 import App from "./App.jsx";
 import { createBrowserRouter, RouterProvider } from "react-router";
+import "bootstrap/dist/css/bootstrap.min.css";
 import LandingPage from "./components/LandingPage.jsx";
 import TestPage from "./components/TestPage.jsx";
-import "bootstrap/dist/css/bootstrap.min.css";
+import DefaultLayout from "./layout/DefaultLayout.jsx";
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <LandingPage />,
-  },
-  {
-    path: "/words",
-    element: <TestPage />,
+    Component: DefaultLayout,
+    children: [
+      { index: true, Component: LandingPage },
+      { path: "words", Component: TestPage },
+      // { path: "translate", Component: Translate },
+    ],
   },
 ]);
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <RouterProvider router={router}>
-      <App />
-    </RouterProvider>
+    <RouterProvider router={router} />
   </StrictMode>
 );
