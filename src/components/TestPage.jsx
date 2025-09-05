07ecf88984930a27480857d7a7b1065b39c29e33
Ronald De Mesa <ronaldreyjr00601201@gmail.com>
2025-09-05 09:50:37 +0800

    add footer into testpage component

--- src/components/TestPage.jsx
+++ src/components/TestPage.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import About from "./About";
 import Table from "./Table";
 import Header from "./Header";
+import Footer from "./Footer";
 
 function TestPage() {
   return (
@@ -12,8 +13,9 @@ function TestPage() {
     <div>
       <About />
       <Table />
-    </div></>
-
+    </div>
+    <Footer/>
+    </>
   );
 }
 
